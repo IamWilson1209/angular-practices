c4350f91cb43b7c9d704794b0e5c2e2c7771c0d0
WeiShiuan <zenfonlee@gmail.com>
2025-05-12 10:31:06 +0800

    add 4-investment-calculator module

---
+++ 4-investment-calculator/module/src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: false,
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  private investmentService = inject(InvestmentService);
+
+  get results() {
+    return this.investmentService.resultsData;
+  }
+
+  /*
+    可以回傳ReadOnly確保不會被修改
+    results = compute(() => this.investmentService.resultData())
+  */
+}
